b02d0a811e4205cbb3fe17128655ccc290f4d59c
yu <2758345412@qq.com>
2024-04-06 23:15:09 +0800

    feat: 修改 Netty 请求包装器

--- YuGateway-Core/src/main/java/com/yu/gateway/core/context/HttpRequestWrapper.java
+++ YuGateway-Core/src/main/java/com/yu/gateway/core/context/HttpRequestWrapper.java
@@ -6,11 +6,11 @@ import lombok.Data;
 
 /**
  * @author yu
- * @description Netty请求包装器：请求+上下文
+ * @description Netty请求包装器：请求 + 上下文
  * @date 2024-04-01
  */
 @Data
 public class HttpRequestWrapper {
     private FullHttpRequest request;
-    private ChannelHandlerContext context;
+    private ChannelHandlerContext ctx;
 }
