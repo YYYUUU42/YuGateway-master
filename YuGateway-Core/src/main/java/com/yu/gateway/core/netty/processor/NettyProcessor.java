5616353f8065f318531ee9bbe351c852a5afd0fb
yu <2758345412@qq.com>
2024-04-07 19:43:01 +0800

    feat: Netty 请求处理器

---
+++ YuGateway-Core/src/main/java/com/yu/gateway/core/netty/processor/NettyProcessor.java
@@ -0,0 +1,28 @@
+package com.yu.gateway.core.netty.processor;
+
+
+import com.yu.gateway.core.context.HttpRequestWrapper;
+
+/**
+ * @author yu
+ * @description Netty请求处理器
+ * @date 2024-04-06
+ */
+public interface NettyProcessor {
+
+    /**
+     * 处理请求
+     * @param wrapper
+     */
+    void process(HttpRequestWrapper wrapper);
+
+    /**
+     * 启动
+     */
+    void  start();
+
+    /**
+     * 关闭
+     */
+    void shutDown();
+}
