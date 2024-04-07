a5b854e94382de9ce525b6c2c87dac30c05f21ed
yu <2758345412@qq.com>
2024-04-07 19:43:20 +0800

    feat: Netty 组件集合封装

---
+++ YuGateway-Core/src/main/java/com/yu/gateway/core/Container.java
@@ -0,0 +1,56 @@
+package com.yu.gateway.core;
+
+import com.yu.gateway.common.constant.GatewayConst;
+import com.yu.gateway.core.netty.NettyHttpClient;
+import com.yu.gateway.core.netty.NettyHttpServer;
+import com.yu.gateway.core.netty.processor.NettyCoreProcessor;
+import com.yu.gateway.core.netty.processor.NettyProcessor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author yu
+ * @description Netty组件集合封装
+ * @date 2024-04-06
+ */
+@Slf4j
+public class Container implements LifeCycle{
+    private final Config config;
+    private NettyHttpServer nettyHttpServer;
+    private NettyHttpClient nettyHttpClient;
+    private NettyProcessor nettyProcessor;
+
+    public Container(Config config) {
+        this.config = config;
+        init();
+    }
+
+    @Override
+    public void init() {
+        NettyCoreProcessor nettyCoreProcessor = new NettyCoreProcessor();
+        // 多生产者多消费者模式
+        if (GatewayConst.BUFFER_TYPE_PARALLEL.equals(config.getBufferType())) {
+//            this.nettyProcessor = new DisruptorNettyCoreProcessor(config, nettyCoreProcessor);
+            this.nettyProcessor = nettyCoreProcessor;
+        } else {
+            this.nettyProcessor = nettyCoreProcessor;
+        }
+        this.nettyHttpServer = new NettyHttpServer(config, nettyProcessor);
+        // nettyClient、nettyServer 公用相同 work_threadGroup
+        this.nettyHttpClient = new NettyHttpClient(config, nettyHttpServer.getEventLoopGroupWorker());
+    }
+
+    @Override
+    public void start() {
+        nettyProcessor.start();
+        nettyHttpServer.start();
+        nettyHttpClient.start();
+        log.info("api gateway starting!");
+    }
+
+    @Override
+    public void shutdown() {
+        nettyProcessor.shutDown();
+        nettyHttpClient.shutdown();
+        nettyHttpServer.shutdown();
+    }
+}
