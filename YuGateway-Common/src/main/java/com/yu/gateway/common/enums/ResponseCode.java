86040b05b35794326554363fab0be7c2476439d7
yu <2758345412@qq.com>
2024-04-29 23:19:15 +0800

    feat: 修改请求响应编码

--- YuGateway-Common/src/main/java/com/yu/gateway/common/enums/ResponseCode.java
+++ YuGateway-Common/src/main/java/com/yu/gateway/common/enums/ResponseCode.java
@@ -28,13 +28,7 @@ public enum ResponseCode {
 
     HTTP_RESPONSE_ERROR(HttpResponseStatus.INTERNAL_SERVER_ERROR, 10030, "服务返回异常"),
 
-    DUBBO_DISPATCH_CONFIG_EMPTY(HttpResponseStatus.INTERNAL_SERVER_ERROR, 10016, "路由配置不能为空"),
-    DUBBO_PARAMETER_TYPE_EMPTY(HttpResponseStatus.BAD_REQUEST, 10017, "请求的参数类型不能为空"),
-    DUBBO_PARAMETER_VALUE_ERROR(HttpResponseStatus.BAD_REQUEST, 10018, "请求参数解析错误"),
-    DUBBO_METHOD_NOT_FOUNT(HttpResponseStatus.NOT_FOUND, 10021, "方法不存在"),
-    DUBBO_CONNECT_ERROR(HttpResponseStatus.INTERNAL_SERVER_ERROR, 10022, "下游服务发生异常,请稍后再试"),
-    DUBBO_REQUEST_ERROR(HttpResponseStatus.INTERNAL_SERVER_ERROR, 10028, "服务请求异常"),
-    DUBBO_RESPONSE_ERROR(HttpResponseStatus.INTERNAL_SERVER_ERROR, 10029, "服务返回异常"),
+    USERID_MISMATCH(HttpResponseStatus.BAD_REQUEST, 10001, "请求参数错误"),
     VERIFICATION_FAILED(HttpResponseStatus.BAD_REQUEST,10030, "请求参数校验失败"),
     BLACKLIST(HttpResponseStatus.FORBIDDEN,10004, "请求IP在黑名单"),
     WHITELIST(HttpResponseStatus.FORBIDDEN,10005, "请求IP不在白名单"),
