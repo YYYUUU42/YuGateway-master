59cd26bde126ac5d6b5b8309dafb2d1857f6ad36
yu <2758345412@qq.com>
2024-05-09 23:57:31 +0800

    feat: 新增url加密测试

--- YuGateway-HttpServer/YuGateway-http-1/src/main/java/com/yu/gateway/httpService/controller/HttpController.java
+++ YuGateway-HttpServer/YuGateway-http-1/src/main/java/com/yu/gateway/httpService/controller/HttpController.java
@@ -5,6 +5,7 @@ import com.yu.gateway.client.api.ApiProperties;
 import com.yu.gateway.client.api.ApiProtocol;
 import com.yu.gateway.client.api.ApiService;
 import com.yu.gateway.common.constant.FilterConst;
+import com.yu.gateway.common.utils.cipher.CryptoHelper;
 import com.yu.gateway.common.utils.cipher.RSAUtil;
 import com.yu.gateway.common.utils.jwt.JWTUtil;
 import com.yu.gateway.common.utils.redis.JedisUtil;
@@ -13,13 +14,17 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.crypto.Cipher;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import java.util.Objects;
+import java.security.KeyFactory;
+import java.security.PublicKey;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.*;
 import java.util.stream.Stream;
-
 
 @RestController
 @ApiService(serviceId = "backend-http-server", protocol = ApiProtocol.HTTP, patternPath = "/http-server/**")
@@ -90,5 +95,58 @@ public class HttpController {
 		log.info("create rsa-publicKey: {}, rsa-privateKey: {}", keyPair.getPublicKey(), keyPair.getPrivateKey());
 
 		return keyPair.getPublicKey();
+	}
+
+
+	/**
+	 * 模拟前端对自己得到的 RSA 公钥进行加密
+	 */
+	@ApiInvoker(path = "/http-server/encrypt-symmetric-key")
+	@GetMapping("/http-server/encrypt-symmetric-key")
+	public String getEncryptSymmetricKey(HttpServletRequest request) throws Exception {
+		String publicKeyPEM = request.getHeader("RSA-secret-key");
+		String symmetricKey = "yu123456";
+
+		byte[] decoded = Base64.getDecoder().decode(publicKeyPEM);
+		KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+		X509EncodedKeySpec keySpec = new X509EncodedKeySpec(decoded);
+		PublicKey publicKey = keyFactory.generatePublic(keySpec);
+
+		byte[] publicKeyBytes = publicKey.getEncoded();
+		String publicKeyBase64 = Base64.getEncoder().encodeToString(publicKeyBytes);
+
+		Cipher cipher = Cipher.getInstance("RSA");
+		cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+
+		return RSAUtil.encryptPublicKey(symmetricKey, publicKeyBase64);
+	}
+
+	/**
+	 * 模拟前端对 url 进行加密测试
+	 */
+	@ApiInvoker(path = "/http-server/encrypt-url")
+	@GetMapping("/http-server/encrypt-url")
+	public String getEncryptURL(HttpServletRequest request) throws Exception {
+		String symmetricPublicKey = "symmetric:key:yu123456";
+		StringBuilder builder = new StringBuilder();
+		String plainText = "productId=1";
+
+		builder.append(plainText);
+		builder.append("&signature=");
+		builder.append(CryptoHelper.encryptParams(plainText, symmetricPublicKey));
+
+		return CryptoHelper.encryptParams(builder.toString(), symmetricPublicKey);
+	}
+
+
+	/**
+	 * 测试 URL 加密
+	 */
+	@ApiInvoker(path = "/http-server/v1/userId")
+	@GetMapping("/http-server/v1/userId")
+	public String testURLEncode(@RequestParam("userId") String productId) {
+		log.info("userId: {}", productId);
+
+		return "url encode test success";
 	}
 }
